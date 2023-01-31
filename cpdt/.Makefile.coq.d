a9d862febdc74789495c836debe121ca704f2c58
Niko Matsakis <niko@alum.mit.edu>
2023-01-31 04:33:49 -0500

    import and build book sources

---
+++ cpdt/.Makefile.coq.d
@@ -0,0 +1,63 @@
+src/CpdtTactics.vo src/CpdtTactics.glob src/CpdtTactics.v.beautified src/CpdtTactics.required_vo: src/CpdtTactics.v 
+src/CpdtTactics.vio: src/CpdtTactics.v 
+src/CpdtTactics.vos src/CpdtTactics.vok src/CpdtTactics.required_vos: src/CpdtTactics.v 
+src/MoreSpecif.vo src/MoreSpecif.glob src/MoreSpecif.v.beautified src/MoreSpecif.required_vo: src/MoreSpecif.v 
+src/MoreSpecif.vio: src/MoreSpecif.v 
+src/MoreSpecif.vos src/MoreSpecif.vok src/MoreSpecif.required_vos: src/MoreSpecif.v 
+src/DepList.vo src/DepList.glob src/DepList.v.beautified src/DepList.required_vo: src/DepList.v src/CpdtTactics.vo
+src/DepList.vio: src/DepList.v src/CpdtTactics.vio
+src/DepList.vos src/DepList.vok src/DepList.required_vos: src/DepList.v src/CpdtTactics.vos
+src/Intro.vo src/Intro.glob src/Intro.v.beautified src/Intro.required_vo: src/Intro.v 
+src/Intro.vio: src/Intro.v 
+src/Intro.vos src/Intro.vok src/Intro.required_vos: src/Intro.v 
+src/StackMachine.vo src/StackMachine.glob src/StackMachine.v.beautified src/StackMachine.required_vo: src/StackMachine.v src/CpdtTactics.vo
+src/StackMachine.vio: src/StackMachine.v src/CpdtTactics.vio
+src/StackMachine.vos src/StackMachine.vok src/StackMachine.required_vos: src/StackMachine.v src/CpdtTactics.vos
+src/InductiveTypes.vo src/InductiveTypes.glob src/InductiveTypes.v.beautified src/InductiveTypes.required_vo: src/InductiveTypes.v src/CpdtTactics.vo
+src/InductiveTypes.vio: src/InductiveTypes.v src/CpdtTactics.vio
+src/InductiveTypes.vos src/InductiveTypes.vok src/InductiveTypes.required_vos: src/InductiveTypes.v src/CpdtTactics.vos
+src/Predicates.vo src/Predicates.glob src/Predicates.v.beautified src/Predicates.required_vo: src/Predicates.v src/CpdtTactics.vo
+src/Predicates.vio: src/Predicates.v src/CpdtTactics.vio
+src/Predicates.vos src/Predicates.vok src/Predicates.required_vos: src/Predicates.v src/CpdtTactics.vos
+src/Coinductive.vo src/Coinductive.glob src/Coinductive.v.beautified src/Coinductive.required_vo: src/Coinductive.v src/CpdtTactics.vo
+src/Coinductive.vio: src/Coinductive.v src/CpdtTactics.vio
+src/Coinductive.vos src/Coinductive.vok src/Coinductive.required_vos: src/Coinductive.v src/CpdtTactics.vos
+src/Subset.vo src/Subset.glob src/Subset.v.beautified src/Subset.required_vo: src/Subset.v src/CpdtTactics.vo
+src/Subset.vio: src/Subset.v src/CpdtTactics.vio
+src/Subset.vos src/Subset.vok src/Subset.required_vos: src/Subset.v src/CpdtTactics.vos
+src/GeneralRec.vo src/GeneralRec.glob src/GeneralRec.v.beautified src/GeneralRec.required_vo: src/GeneralRec.v src/CpdtTactics.vo src/Coinductive.vo
+src/GeneralRec.vio: src/GeneralRec.v src/CpdtTactics.vio src/Coinductive.vio
+src/GeneralRec.vos src/GeneralRec.vok src/GeneralRec.required_vos: src/GeneralRec.v src/CpdtTactics.vos src/Coinductive.vos
+src/MoreDep.vo src/MoreDep.glob src/MoreDep.v.beautified src/MoreDep.required_vo: src/MoreDep.v src/CpdtTactics.vo src/MoreSpecif.vo
+src/MoreDep.vio: src/MoreDep.v src/CpdtTactics.vio src/MoreSpecif.vio
+src/MoreDep.vos src/MoreDep.vok src/MoreDep.required_vos: src/MoreDep.v src/CpdtTactics.vos src/MoreSpecif.vos
+src/DataStruct.vo src/DataStruct.glob src/DataStruct.v.beautified src/DataStruct.required_vo: src/DataStruct.v src/CpdtTactics.vo
+src/DataStruct.vio: src/DataStruct.v src/CpdtTactics.vio
+src/DataStruct.vos src/DataStruct.vok src/DataStruct.required_vos: src/DataStruct.v src/CpdtTactics.vos
+src/Equality.vo src/Equality.glob src/Equality.v.beautified src/Equality.required_vo: src/Equality.v src/CpdtTactics.vo
+src/Equality.vio: src/Equality.v src/CpdtTactics.vio
+src/Equality.vos src/Equality.vok src/Equality.required_vos: src/Equality.v src/CpdtTactics.vos
+src/Generic.vo src/Generic.glob src/Generic.v.beautified src/Generic.required_vo: src/Generic.v src/CpdtTactics.vo src/DepList.vo
+src/Generic.vio: src/Generic.v src/CpdtTactics.vio src/DepList.vio
+src/Generic.vos src/Generic.vok src/Generic.required_vos: src/Generic.v src/CpdtTactics.vos src/DepList.vos
+src/Universes.vo src/Universes.glob src/Universes.v.beautified src/Universes.required_vo: src/Universes.v src/DepList.vo src/CpdtTactics.vo
+src/Universes.vio: src/Universes.v src/DepList.vio src/CpdtTactics.vio
+src/Universes.vos src/Universes.vok src/Universes.required_vos: src/Universes.v src/DepList.vos src/CpdtTactics.vos
+src/LogicProg.vo src/LogicProg.glob src/LogicProg.v.beautified src/LogicProg.required_vo: src/LogicProg.v src/CpdtTactics.vo
+src/LogicProg.vio: src/LogicProg.v src/CpdtTactics.vio
+src/LogicProg.vos src/LogicProg.vok src/LogicProg.required_vos: src/LogicProg.v src/CpdtTactics.vos
+src/Match.vo src/Match.glob src/Match.v.beautified src/Match.required_vo: src/Match.v src/CpdtTactics.vo
+src/Match.vio: src/Match.v src/CpdtTactics.vio
+src/Match.vos src/Match.vok src/Match.required_vos: src/Match.v src/CpdtTactics.vos
+src/Reflection.vo src/Reflection.glob src/Reflection.v.beautified src/Reflection.required_vo: src/Reflection.v src/CpdtTactics.vo src/MoreSpecif.vo
+src/Reflection.vio: src/Reflection.v src/CpdtTactics.vio src/MoreSpecif.vio
+src/Reflection.vos src/Reflection.vok src/Reflection.required_vos: src/Reflection.v src/CpdtTactics.vos src/MoreSpecif.vos
+src/Large.vo src/Large.glob src/Large.v.beautified src/Large.required_vo: src/Large.v src/CpdtTactics.vo src/MoreDep.vo
+src/Large.vio: src/Large.v src/CpdtTactics.vio src/MoreDep.vio
+src/Large.vos src/Large.vok src/Large.required_vos: src/Large.v src/CpdtTactics.vos src/MoreDep.vos
+src/ProgLang.vo src/ProgLang.glob src/ProgLang.v.beautified src/ProgLang.required_vo: src/ProgLang.v src/CpdtTactics.vo src/DepList.vo
+src/ProgLang.vio: src/ProgLang.v src/CpdtTactics.vio src/DepList.vio
+src/ProgLang.vos src/ProgLang.vok src/ProgLang.required_vos: src/ProgLang.v src/CpdtTactics.vos src/DepList.vos
+src/Conclusion.vo src/Conclusion.glob src/Conclusion.v.beautified src/Conclusion.required_vo: src/Conclusion.v 
+src/Conclusion.vio: src/Conclusion.v 
+src/Conclusion.vos src/Conclusion.vok src/Conclusion.required_vos: src/Conclusion.v 
