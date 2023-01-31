fda3c3a6acaece029b4a7ac086556c078121cee6
Niko Matsakis <niko@alum.mit.edu>
2023-01-31 05:23:13 -0500

    work through page 26

--- cpdt-chapters/chapter2.v
+++ cpdt-chapters/chapter2.v
@@ -1,4 +1,4 @@
-Require Import Bool Arith List CpdtTactics.
+Require Import Bool Arith List Cpdt.CpdtTactics.
 Set Implicit Arguments.
 
 Inductive binop : Set := Plus | Times.
@@ -13,7 +13,7 @@ Definition binopDenote (b : binop) : nat -> nat -> nat :=
     | Times => mult
     end.
 
-Definition expDenote (e : exp) : nat :=
+Fixpoint expDenote (e : exp) : nat :=
     match e with
     | Const n => n
     | Binop b e1 e2 => (binopDenote b) (expDenote e1) (expDenote e2)
@@ -25,7 +25,7 @@ Eval simpl in expDenote (Const 42).
 
 Inductive instr : Set :=
 | iConst : nat -> instr
-| iBinop : binop -> inst.
+| iBinop : binop -> instr.
 
 Definition prog := list instr.
 
@@ -33,7 +33,7 @@ Definition stack := list nat.
 
 Definition instrDenote (i: instr) (s : stack) : option stack :=
     match i with
-    | iConst n => (Some n :: s)
+    | iConst n => Some (n :: s)
     | iBinop b =>
         match s with
         | arg1 :: arg2 :: s' =>
@@ -45,4 +45,27 @@ Definition instrDenote (i: instr) (s : stack) : option stack :=
 
 Fixpoint progDenote (p : prog) (s : stack) : option stack :=
     match p with
-    
+    | nil => Some s
+    | i :: p' =>
+        match instrDenote i s with
+        | None => None
+        | Some s' => progDenote p' s'
+        end
+    end.
+
+Fixpoint compile (e : exp) : prog :=
+    match e with
+    | Const n => iConst n :: nil
+    | Binop b e1 e2 => compile e2 ++ compile e1 ++ iBinop b :: nil
+    end.
+
+Eval simpl in (compile (Const 42)).
+
+(* "Compiling e and executing it is the same as
+   pushing the value of expDonte on the stack" *)
+Lemma compile_correct' : forall e p s, 
+    progDenote (compile e ++ p) s = progDenote p (expDenote e :: s).
+    induction e; crush.
+
+Theorem compile_correct : 
+    forall e, progDenote (compile e) nil = Some (expDenote e :: nil).
